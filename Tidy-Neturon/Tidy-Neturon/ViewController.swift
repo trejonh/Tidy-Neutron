75f8f1ccedbf587196ba1ee3a5753b58f9f1eb9b
TreJon House <trejon_house@yahoo.com>
2017-03-27 14:23:06 -0500

    got a launch screen and main storyboard

---
+++ Tidy-Neturon/Tidy-Neturon/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Tidy-Neturon
+//
+//  Created by Trejon House on 3/26/17.
+//  Copyright © 2017 TreJon House. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //MARK: Properties
+    
+    @IBOutlet weak var welcomeBanner: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    //MARK: Actions
+    
+    @IBAction func connectToDevice(sender: UIButton) {
+        welcomeBanner.text = "connect to device"
+    }
+    
+    @IBAction func changeDeviceConfig(sender: UIButton) {
+        welcomeBanner.text = "change device config"
+    }
+}
+
